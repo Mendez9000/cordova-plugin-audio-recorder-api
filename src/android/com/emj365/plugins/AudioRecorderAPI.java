a1c62b16e48cb4718f4f46f0be0c22c11fdc9063
Mendez9000 <sergio.mendez.uruguay@gmail.com>
2016-08-07 19:52:43 -0300

    Update AudioRecorderAPI.java

--- src/android/com/emj365/plugins/AudioRecorderAPI.java
+++ src/android/com/emj365/plugins/AudioRecorderAPI.java
@@ -38,7 +38,7 @@ public class AudioRecorderAPI extends CordovaPlugin {
       myRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
       myRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
       myRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-      myRecorder.setAudioSamplingRate(44100);
+      myRecorder.setAudioSamplingRate(8000);
       myRecorder.setAudioChannels(1);
       myRecorder.setAudioEncodingBitRate(32000);
       myRecorder.setOutputFile(outputFile);

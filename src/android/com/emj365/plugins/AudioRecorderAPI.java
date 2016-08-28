6523b89e4a60dcec4117fa34ad8563ee6c18bf59
Mendez9000 <sergio.mendez.uruguay@gmail.com>
2016-08-28 19:42:16 -0300

    Update AudioRecorderAPI.java

--- src/android/com/emj365/plugins/AudioRecorderAPI.java
+++ src/android/com/emj365/plugins/AudioRecorderAPI.java
@@ -30,7 +30,7 @@ import android.media.MediaRecorder;
 
 
 public class AudioRecorderAPI extends CordovaPlugin {
-
+  private static final int RECORDER_BPP = 16;
   private static final int RECORDER_SAMPLERATE = 8000;
   private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
   private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;

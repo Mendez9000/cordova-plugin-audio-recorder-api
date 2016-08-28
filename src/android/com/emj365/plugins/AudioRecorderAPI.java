422533f2f7ad73e9e8ea9f4794b666545ac34f9d
Mendez9000 <sergio.mendez.uruguay@gmail.com>
2016-08-28 19:40:30 -0300

    Update AudioRecorderAPI.java

--- src/android/com/emj365/plugins/AudioRecorderAPI.java
+++ src/android/com/emj365/plugins/AudioRecorderAPI.java
@@ -41,6 +41,7 @@ public class AudioRecorderAPI extends CordovaPlugin {
   private CountDownTimer countDowntimer;
   private boolean isRecording = false;
   private String outputFile=null;
+  private String outputFileTmp=null;
   private String error= "";
 
   @Override
@@ -54,7 +55,8 @@ public class AudioRecorderAPI extends CordovaPlugin {
       seconds = 7;
     }
     if (action.equals("record")) {
-        outputFile = context.getFilesDir().getAbsoluteFile() + "/" + UUID.randomUUID().toString() + ".pcm";
+        outputFileTmp = context.getFilesDir().getAbsoluteFile() + "/" + UUID.randomUUID().toString() + ".pcm";
+        outputFile = context.getFilesDir().getAbsoluteFile() + "/" + UUID.randomUUID().toString() + ".wav";
         recorder = new AudioRecord(MediaRecorder.AudioSource.MIC,
             RECORDER_SAMPLERATE, RECORDER_CHANNELS,
             RECORDER_AUDIO_ENCODING, BufferElements2Rec * BytesPerElement);
@@ -108,6 +110,8 @@ public class AudioRecorderAPI extends CordovaPlugin {
     }
     isRecording = false;
     
+    copyWaveFile(outputFileTmp, outputFile);
+    
     cordova.getThreadPool().execute(new Runnable() {
       public void run() {
         error+= "..PASO-10";
@@ -137,7 +141,7 @@ private void writeAudioDataToFile() {
     FileOutputStream os = null;
     try {
        error+= "..PASO2";
-        os = new FileOutputStream(outputFile);
+        os = new FileOutputStream(outputFileTmp);
     } catch (FileNotFoundException e) {
        error+= "..ERROR0";
         e.printStackTrace();
@@ -171,4 +175,96 @@ private void writeAudioDataToFile() {
         e.printStackTrace();
     }
   }
+  
+  private void copyWaveFile(String inFilename,String outFilename){
+         FileInputStream in = null;
+         FileOutputStream out = null;
+         long totalAudioLen = 0;
+         long totalDataLen = totalAudioLen + 36;
+         long longSampleRate = RECORDER_SAMPLERATE;
+         int channels = 2;
+         long byteRate = RECORDER_BPP * RECORDER_SAMPLERATE * channels/8;
+
+         byte[] data = new byte[bufferSize];
+
+         try {
+             in = new FileInputStream(inFilename);
+             out = new FileOutputStream(outFilename);
+             totalAudioLen = in.getChannel().size();
+             totalDataLen = totalAudioLen + 36;
+
+            
+
+             WriteWaveFileHeader(out, totalAudioLen, totalDataLen,
+                          longSampleRate, channels, byteRate);
+
+             while(in.read(data) != -1) {
+                 out.write(data);
+             }
+
+             in.close();
+             out.close();
+         } catch (FileNotFoundException e) {
+             e.printStackTrace();
+         } catch (IOException e) {
+             e.printStackTrace();
+         }
+     }
+  
+   private void WriteWaveFileHeader(
+         FileOutputStream out, long totalAudioLen,
+         long totalDataLen, long longSampleRate, int channels,
+         long byteRate) throws IOException
+     {
+         byte[] header = new byte[44];
+
+         header[0] = 'R';  // RIFF/WAVE header
+         header[1] = 'I';
+         header[2] = 'F';
+         header[3] = 'F';
+         header[4] = (byte) (totalDataLen & 0xff);
+         header[5] = (byte) ((totalDataLen >> 8) & 0xff);
+         header[6] = (byte) ((totalDataLen >> 16) & 0xff);
+         header[7] = (byte) ((totalDataLen >> 24) & 0xff);
+         header[8] = 'W';
+         header[9] = 'A';
+         header[10] = 'V';
+         header[11] = 'E';
+         header[12] = 'f';  // 'fmt ' chunk
+         header[13] = 'm';
+         header[14] = 't';
+         header[15] = ' ';
+         header[16] = 16;  // 4 bytes: size of 'fmt ' chunk
+         header[17] = 0;
+         header[18] = 0;
+         header[19] = 0;
+         header[20] = 1;  // format = 1
+         header[21] = 0;
+         header[22] = (byte) channels;
+         header[23] = 0;
+         header[24] = (byte) (longSampleRate & 0xff);
+         header[25] = (byte) ((longSampleRate >> 8) & 0xff);
+         header[26] = (byte) ((longSampleRate >> 16) & 0xff);
+         header[27] = (byte) ((longSampleRate >> 24) & 0xff);
+         header[28] = (byte) (byteRate & 0xff);
+         header[29] = (byte) ((byteRate >> 8) & 0xff);
+         header[30] = (byte) ((byteRate >> 16) & 0xff);
+         header[31] = (byte) ((byteRate >> 24) & 0xff);
+         header[32] = (byte) (2 * 16 / 8);  // block align
+         header[33] = 0;
+         header[34] = RECORDER_BPP;  // bits per sample
+         header[35] = 0;
+         header[36] = 'd';
+         header[37] = 'a';
+         header[38] = 't';
+         header[39] = 'a';
+         header[40] = (byte) (totalAudioLen & 0xff);
+         header[41] = (byte) ((totalAudioLen >> 8) & 0xff);
+         header[42] = (byte) ((totalAudioLen >> 16) & 0xff);
+         header[43] = (byte) ((totalAudioLen >> 24) & 0xff);
+
+         out.write(header, 0, 44);
+     }
+  
+  
 }

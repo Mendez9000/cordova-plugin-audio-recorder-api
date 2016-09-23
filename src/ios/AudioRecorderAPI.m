1646c5a6a1a243e4d1b85a8a61c212f7bf2dd8e3
Mendez9000 <sergio.mendez.uruguay@gmail.com>
2016-09-23 00:25:59 -0300

    Update AudioRecorderAPI.m

--- src/ios/AudioRecorderAPI.m
+++ src/ios/AudioRecorderAPI.m
@@ -14,6 +14,7 @@
   
    NSData *data = [NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://soundbible.com/mp3/A-Tone-His_Self-1266414414.mp3"]];
    AVAudioPlayer *audio = [[AVAudioPlayer alloc] initWithData:data error:nil];
+   [audio setVolume: 1.0];
    [audio play];
 
     AVAudioSession *audioSession = [AVAudioSession sharedInstance];

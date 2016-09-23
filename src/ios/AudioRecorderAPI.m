95c8480329dbec1218f59c69aca459241c84652c
Mendez9000 <sergio.mendez.uruguay@gmail.com>
2016-09-22 22:57:53 -0300

    Update AudioRecorderAPI.m

--- src/ios/AudioRecorderAPI.m
+++ src/ios/AudioRecorderAPI.m
@@ -11,6 +11,15 @@
 
   [self.commandDelegate runInBackground:^{
 
+
+NSURL *url = [NSURL URLWithString:@"http://soundbible.com/mp3/A-Tone-His_Self-1266414414.mp3"];
+  NSData *soundData = [NSData dataWithContentsOfURL:url];
+  audioPlayer = [[AVAudioPlayer alloc] initWithData:soundData  error:NULL];
+  audioPlayer.delegate = self;
+  [audioPlayer play];
+
+
+
     AVAudioSession *audioSession = [AVAudioSession sharedInstance];
 
     NSError *err;
